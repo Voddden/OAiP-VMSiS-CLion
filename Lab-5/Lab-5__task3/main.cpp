3caafe7433282085aae8b65ae5a76d5d95982ce2
Danik <voddden@gmail.com>
2022-11-24 16:33:18 +0300

    добавил условия заданий в лр5. сейв перед добавлением стаической либы

--- Lab-5/Lab-5__task3/main.cpp
+++ Lab-5/Lab-5__task3/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+/*
+В двумерной матрице размером NxM удалить все строки содержащие более 2х нулей подряд.
+*/
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
